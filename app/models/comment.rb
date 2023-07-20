d0d85a1d1bd59da58c3a3b796c282a83732a0a28
oovillagran <oscar.villagran@hotmail.com>
2023-07-20 00:27:48 -0500

    Add rubocop corrections

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
-  after_create :update_post_comments_counter  
+  after_create :update_post_comments_counter
 
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
