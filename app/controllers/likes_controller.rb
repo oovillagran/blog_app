17f117ee5415acb30170aab38a8530d7f5e6900d
oovillagran <oscar.villagran@hotmail.com>
2023-07-27 12:48:11 -0500

    Add create method for Likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.fin(params[:post_id])
+    @like = @post.likes.build(author: current_user)
+
+    if @like.save
+      redirect_to user_post_path(current_user, @post), notice: 'Liked the post!'
+    else
+      redirect_to user_post_path(current_user, @post), alert: 'Failed to like the post.'
+    end
+  end
 end
