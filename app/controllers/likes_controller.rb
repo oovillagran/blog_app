f5557e8d19671a3ae0a7512feb6bd581896fe9b3
oovillagran <oscar.villagran@hotmail.com>
2023-07-27 14:15:07 -0500

    Add user likes validation

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,20 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.build(author: current_user)
+    existing_like = @post.likes.find_by(author: current_user)
 
-    if @like.save
-      redirect_to user_post_path(current_user, @post), notice: 'Liked the post!'
+    if existing_like
+      flash[:alert] = "You have already liked this post."
+      redirect_to user_post_path(@post.author, @post)
     else
-      redirect_to user_post_path(current_user, @post), alert: 'Failed to like the post.'
+      @like = current_user.likes.new(post: @post)
+      if @like.save
+        flash[:notice] = "You liked the post successfully."
+        redirect_to user_post_path(@post.author, @post)
+      else
+        flash[:alert] = "Something went wrong. Please try again."
+        redirect_to user_post_path(@post.author, @post)
+      end
     end
   end
 end
