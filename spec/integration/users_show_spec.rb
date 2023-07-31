d604b1b051d6e09934998c480a7fc30c382c1bb9
oovillagran <oscar.villagran@hotmail.com>
2023-07-31 17:34:54 -0500

    Add tests for users_show_spec.rb

--- spec/integration/users_show_spec.rb
+++ spec/integration/users_show_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
-Rspec.feature 'UsersShow', type: :feature do
-    
+RSpec.feature 'UsersShow', type: :feature do
+  before do
+    @user = User.create(name: 'John', photo: 'user1.jpg', bio: 'I am John')
+    visit user_path(@user)
+  end
+
+  scenario "shows the user's profile picture" do
+    expect(page).to have_selector("img[src$='#{@user.photo}']")
+  end
+
+  scenario "shows the user's name" do
+    expect(page).to have_content(@user.name)
+  end
+
+  scenario "shows the user's number of posts" do
+    expect(page).to have_content("Number of Posts: #{@user.posts_counter}")
+  end
+
+  scenario 'shows the user bio' do
+    expect(page).to have_content(@user.bio)
+  end
+end
